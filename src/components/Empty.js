cd3bfdb9e40e9052b5415801686dcbefe87ab31c
SvetozarMiskovic <svetomiskovic98@gmail.com>
2021-12-07 13:53:06 +0100

    added Empty component, changed the CSS a bit

---
+++ src/components/Empty.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/Empty.css';
+function Empty() {
+  return (
+    <div className="empty">
+      <h2>There are no lists created!</h2>
+    </div>
+  );
+}
+
+export default Empty;
--- src/components/TaskHolder.js
+++ src/components/TaskHolder.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import '../styles/TaskHolder.css';
+import Empty from './Empty';
 import SingleList from './SingleList';
 function TaskHolder(props) {
-  return (
-    <div className="task-holder">
-      {props.lists.map(list => {
-        return (
-          <SingleList
-            removeTodo={props.removeTodo}
-            removeList={props.removeList}
-            key={list.id}
-            listDetails={list}
-          />
-        );
-      })}
-    </div>
-  );
+  if (props.lists.length === 0) {
+    return <Empty />;
+  } else
+    return (
+      <div className="task-holder">
+        {props.lists.map(list => {
+          return (
+            <SingleList
+              removeTodo={props.removeTodo}
+              removeList={props.removeList}
+              key={list.id}
+              listDetails={list}
+            />
+          );
+        })}
+      </div>
+    );
 }
 
 export default TaskHolder;
